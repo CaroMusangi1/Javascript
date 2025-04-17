04464f249aa6981cbfd2b004616303962ca98ff5
CaroMusangi1 <kitongacarol8@gmail.com>
2025-04-17 21:37:45 +0300

    switch case

--- Hero.js
+++ Hero.js
@@ -105,4 +105,127 @@ console.log("before : " + JSON.stringify(allclothes));  //output = remains uncha
 console.log(myWears(allclothes, 5));//notice skirt becomes reference to allclothes
 console.log("after :" + JSON.stringify(allclothes)); //all changes take place here.5 is pushed and one removed;skirt.push,skirt.shift ; [2,3,4,5]
 
-//
+//BOOLEN and IF
+function trueOrFalse(Aretheromourstrue ){
+  if (Aretheromourstrue){
+  return "Yes they are true";
+}
+return "No,they are false";
+}
+console.log(trueOrFalse(false));//Out put No ,They are false.
+
+//true/false 
+function lessOr(a,b){
+  return a < b;
+}
+console.log(lessOr(26,15)); //false
+console.log(lessOr(15,26));//True
+
+
+//more example on if
+
+function equalTo(val){
+  if (val == 10){
+    return "Equal to"
+  }
+return "Not Equal to"
+}
+console.log(equalTo(11)); //11 != 10
+
+// strict equality sign ===
+//strict ineqeality sign !==
+
+//Greater or less than
+function greaterThan(val){
+  if (val > 1000){
+    return "Greater than 1000";
+  }
+  if (val > 10){
+    return "Greater than 10";
+  }
+  return "less than 10"
+}
+console.log(greaterThan(7));
+console.log (greaterThan(1001));
+
+//Avoiding nested if using 0r and and
+function andIf(val){
+  if (val <= 50 && val >=25){
+    return "inside range"
+  }
+  return "outside range"
+}
+console.log (andIf(27)); // AND 
+
+//OR with <,>
+function orIf(val){
+  if (val > 0 || val < 25){
+    return "inside range"
+  }
+  return "outside range"
+}
+console.log (orIf(67)); //OR
+
+//else and else if using studentmarks
+function student_marks(val){
+  if (val >= 70 && val ==100){
+    return "A"
+  }
+  else if (val >= 60 && val < 70){
+    return "B"
+  }
+  else if (val >= 50 && val < 60){
+    return "C"
+  }
+  else if (val >= 40 && val < 50){
+    return "D"
+  }
+  else 
+  return "Why are you in campus?"
+}
+console.log (student_marks(56)); //C
+console.log (student_marks(28)); //Wafanyaje Campus?
+
+//Use switch? yes
+function switchCase(val){
+  var answer = "";
+  switch (val){
+    case 1:
+      answer =  "Dark";
+      break;
+      case 2:
+        answer = "Lighter";
+        break;
+        case 3:
+          answer = "Blue";
+        break;
+        case 4:
+          answer = "white"
+          break;
+          default: //acts like else
+          answer = "else"
+          break;
+  }
+  return answer;
+}
+console.log(switchCase(3));
+console.log(switchCase(7));
+
+//more on switch
+function cases(val){
+  var answer = "";
+  switch (val) {
+  case 1:
+  case 2:
+  case 3:
+  answer = "mid" //1,2 or
+  break;
+  case 5:
+    case 6:
+      case 7:
+        answer = "high" //5,6 r7
+        break;
+  }
+        return answer;
+}
+        console.log(cases(7)); //output mid
